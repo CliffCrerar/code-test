c69ecf62f3ca5cda913a4b0f83089a97b9c5de62
Cliff Crerar <cliff.crerar@gmail.com>
2019-10-28 21:46:03 +0200

    Wrote ikm test for emerge

--- old-tests/app-jquery-ikm.js
+++ old-tests/app-jquery-ikm.js
@@ -6,10 +6,36 @@ const run = 'p2';
 
 const problems = {
     p1: function () {
+        console.log('problem 1')
+        var y;
+        function modifyVariables(x) {
+            var z = 5;
+            x += 2;
+            y += x + z;
+        }
+
+        var x = 1;
+        var y = 2;
+        var z = 3;
+        modifyVariables(x);
+        document.writeln(x);
+        document.writeln(y);
+        document.writeln(z);
 
     },
     p2: function () {
+        console.log('problem 2')
 
+        // var A = new Array(5);
+        // var A = new Array("A", "2", "C");
+        
+        // var A = new A("A", "B", "C");
+
+        // var A = new Array[];
+
+        var A = Array(0, 1, 2);
+        
+        console.log('A: ', A);
 
     },
     p3: function () {
